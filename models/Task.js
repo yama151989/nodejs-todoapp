8a7beaafd26338ef50c8c367695674892fe79323
yama151989 <yama151989@gmail.com>
2023-05-01 17:41:04 +0900

    deploy

---
+++ models/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "タスク名を入れてください。"],
+        trim: true,
+        maxlength: [20, "タスク名は20文字以内で入力してください。"]
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+});
+
+//「TaskSchema」をどこでも使える
+module.exports = mongoose.model("Task", TaskSchema);
+
